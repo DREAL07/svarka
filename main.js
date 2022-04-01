32aa36002a96dd12547e72fb9d8d33b80fd50cb5
VICTOR-81 <v161181@gmail.com>
2022-04-01 13:38:37 +0700

    start

---
+++ main.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
